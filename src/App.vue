8ef60fdb7a28a24b726e677104e5391861659e5f
Haley Swain <haley.swain@pollinate.com>
2018-04-08 12:02:35 -0700

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="portfolio__greeting">
+    <h2>{{title}}</h2>
+    <p>{{greeting()}}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'nice to meet you'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'welcome to my portfolio'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
